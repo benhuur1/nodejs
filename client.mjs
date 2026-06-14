d945cad9741e649a437210f19ad8b973ce252ab2
José Ben Hur <joseben.hur.86@gmail.com>
2026-06-14 12:16:00 -0300

    Aula sobre o response do nodejs
    Aprendi sobre como funciona as rotas para disponibilizar o recurso na api e depois utilizar no frontend

--- client.mjs
+++ client.mjs
@@ -1,13 +1,6 @@
-const response = await fetch('http://localhost:3000/produtos', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({username: 'José', password: '123456'}),
+const response = await fetch('http://localhost:3000/produto/notebook', {
+    method: 'GET',
 });
 
-console.log(response)
-
-const body = await response.json();
-
-console.log(body);
+const body = await response.text();
+console.log(body)
