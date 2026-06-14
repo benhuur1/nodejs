fecbabb7606ce5f9088bc50bc63db331c67509d7
José Ben Hur <joseben.hur.86@gmail.com>
2026-06-14 11:36:29 -0300

    0203 Response

---
+++ server.mjs
@@ -0,0 +1,42 @@
+import { createServer } from 'node:http'
+
+const server = createServer( async (req, res) => {
+    const url = new URL(req.url, 'http://localhost')
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, Authorization')
+
+    const chunks = []
+    for await (const chunk of req){
+        chunks.push(chunk)
+    }
+    console.log(Buffer.concat(chunks).toString('utf-8'))
+
+    console.log(req.url);
+    if(req.method === 'GET' && url.pathname === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(`
+            <html>
+            <head>
+            <title>Mundo</title>
+            </head>
+            <body>
+            <h1>Olá mundo</h1>
+            </body>
+            </html>
+            `);
+    } else if( req.method === 'POST' && url.pathname === '/produtos'){
+        res.statusCode = 201
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({nome: 'Notebook'}));
+    } else {
+        res.statusCode = 404;
+        res.end('Página não encontrada.')
+    }
+    console.log(req.method)
+});
+
+server.listen(3000, () => {
+    console.log('Server: http://localhost:3000');
+})
